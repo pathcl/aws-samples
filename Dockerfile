4d62389e6f846c627b41a272a056994a7818f04d
pathcl <luis@sanmartin.io>
2024-06-19 22:16:42 +0200

    debug

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,6 @@
 
 FROM golang:1.21.5 AS build
 
-ARG garm_repo=https://github.com/pathcl/aws-samples
-ARG garm_repo_ref=v0.1.4
-
-# build garm binary
-# primarly used to get the binary build for
-# the local CPU architecture
-RUN git clone $garm_repo garm_repo && \
-  cd garm_repo && \
-  git checkout $garm_repo_ref && \
-  CGO_ENABLED=1 go install -ldflags "-linkmode external -extldflags '-static' -X main.Version=$garm_repo_ref" ./cmd/garm && \
-  CGO_ENABLED=1 go install -ldflags "-linkmode external -extldflags '-static' -X main.Version=$garm_repo_ref" ./cmd/garm-cli
-
 RUN git clone https://github.com/pathcl/aws-samples aws-samples && \
   cd aws-samples && \
   git rev-parse HEAD && \
@@ -28,7 +16,7 @@ ENV PATH="${PATH}:/opt/garm/bin"
 
 COPY --from=build /go/bin/test-iam ./bin/test-iam
 
-COPY ./bin/test-iam ./bin/test-iam
+# COPY ./bin/test-iam ./bin/test-iam
 
 RUN chmod +x ./bin/test-iam && \
   apt-get update && \ 
@@ -41,4 +29,4 @@ USER 65543:65543
 
 EXPOSE 9997
 
-ENTRYPOINT ["sleep", "10h"]
+ENTRYPOINT ["/bin/bash"]
--- main.go
+++ main.go
@@ -1,28 +1,105 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX - License - Identifier: Apache - 2.0
+// snippet-start:[ec2.go-v2.CreateInstance]
 package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
+// EC2CreateInstanceAPI defines the interface for the RunInstances and CreateTags functions.
+// We use this interface to test the functions using a mocked service.
+type EC2CreateInstanceAPI interface {
+	RunInstances(ctx context.Context,
+		params *ec2.RunInstancesInput,
+		optFns ...func(*ec2.Options)) (*ec2.RunInstancesOutput, error)
+
+	CreateTags(ctx context.Context,
+		params *ec2.CreateTagsInput,
+		optFns ...func(*ec2.Options)) (*ec2.CreateTagsOutput, error)
+}
+
+// MakeInstance creates an Amazon Elastic Compute Cloud (Amazon EC2) instance.
+// Inputs:
+//     c is the context of the method call, which includes the AWS Region.
+//     api is the interface that defines the method call.
+//     input defines the input arguments to the service call.
+// Output:
+//     If success, a RunInstancesOutput object containing the result of the service call and nil.
+//     Otherwise, nil and an error from the call to RunInstances.
+func MakeInstance(c context.Context, api EC2CreateInstanceAPI, input *ec2.RunInstancesInput) (*ec2.RunInstancesOutput, error) {
+	return api.RunInstances(c, input)
+}
+
+// MakeTags creates tags for an Amazon Elastic Compute Cloud (Amazon EC2) instance.
+// Inputs:
+//     c is the context of the method call, which includes the AWS Region.
+//     api is the interface that defines the method call.
+//     input defines the input arguments to the service call.
+// Output:
+//     If success, a CreateTagsOutput object containing the result of the service call and nil.
+//     Otherwise, nil and an error from the call to CreateTags.
+func MakeTags(c context.Context, api EC2CreateInstanceAPI, input *ec2.CreateTagsInput) (*ec2.CreateTagsOutput, error) {
+	return api.CreateTags(c, input)
+}
+
 func main() {
-	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithClientLogMode(aws.LogRequestWithBody|aws.LogResponseWithBody))
+	name := flag.String("n", "", "The name of the tag to attach to the instance")
+	value := flag.String("v", "", "The value of the tag to attach to the instance")
+	flag.Parse()
+
+	if *name == "" || *value == "" {
+		fmt.Println("You must supply a name and value for the tag (-n NAME -v VALUE)")
+		return
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		panic("configuration error, " + err.Error())
 	}
 
+	minMaxCount := int32(1)
+
 	client := ec2.NewFromConfig(cfg)
-	result, err := client.DescribeInstances(context.Background(), &ec2.DescribeInstancesInput{})
-	if err != nil {
-		panic("error listing instances, " + err.Error())
+	input := &ec2.RunInstancesInput{
+		ImageId:      aws.String("ami-0595d6e81396a9efb"),
+		InstanceType: types.InstanceTypeT2Micro,
+		MinCount:     &minMaxCount,
+		MaxCount:     &minMaxCount,
 	}
 
-	for _, reservation := range result.Reservations {
-		for _, instance := range reservation.Instances {
-			println("Instance ID: " + *instance.InstanceId)
-		}
+	result, err := MakeInstance(context.TODO(), client, input)
+	if err != nil {
+		fmt.Println("Got an error creating an instance:")
+		fmt.Println(err)
+		return
 	}
+
+	tagInput := &ec2.CreateTagsInput{
+		Resources: []string{*result.Instances[0].InstanceId},
+		Tags: []types.Tag{
+			{
+				Key:   name,
+				Value: value,
+			},
+		},
+	}
+
+	_, err = MakeTags(context.TODO(), client, tagInput)
+	if err != nil {
+		fmt.Println("Got an error tagging the instance:")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Created tagged instance with ID " + *result.Instances[0].InstanceId)
 }
+
+// snippet-end:[ec2.go-v2.CreateInstance]
